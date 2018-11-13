4231b2d46c4a3756c6e673037a3fbb2a5677244a
Kevin Luang-orn <kluangorn@gmail.com>
2018-11-13 11:46:44 -0800

    wip updated with react-bootstrap

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,14 +1,49 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
+// const Index = () => <h2>Home</h2>;
+// const About = () => <h2>About</h2>;
+// const Users = () => <h2>Users</h2>;
 
 const AppRouter = () => (
   <Router>
     <div>
-      <nav>
+    <Navbar inverse collapseOnSelect>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#brand">Wedding Developer</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavItem eventKey={1} href="#">
+        Link
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Link
+      </NavItem>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+    <Nav pullRight>
+      <NavItem eventKey={1} href="#">
+        Create Account
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Sign In
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>;
+
+      {/* <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -20,11 +55,12 @@ const AppRouter = () => (
             <Link to="/users/">Users</Link>
           </li>
         </ul>
-      </nav>
+      </nav> */}
 
-      <Route path="/" exact component={Index} />
+      {/* <Route path="/" exact component={Index} />
       <Route path="/about/" component={About} />
-      <Route path="/users/" component={Users} />
+      <Route path="/users/" component={Users} /> */}
+    
     </div>
   </Router>
 );
