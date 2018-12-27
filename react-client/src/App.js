ecdf3d7ddb4970b1ac1d90911e1bccfc5a3e5e27
Kevin Luang-orn <kluangorn@gmail.com>
2018-12-26 22:04:18 -0800

    updated events

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const Index = () => <h2>Home</h2>;
 const Register = () => <h2>Register</h2>;
 const Users = () => <h2>Users</h2>;
+const Events = () => <h2>Eventss</h2>;
 
 const AppRouter = () => (
   <Router>
@@ -19,12 +20,16 @@ const AppRouter = () => (
           <li>
             <Link to="/users/">Users</Link>
           </li>
+          <li>
+            <Link to="/events/">Events</Link>
+          </li>
         </ul>
       </nav>
 
       <Route path="/" exact component={Index} />
       <Route path="/register/" component={Register} />
       <Route path="/users/" component={Users} />
+      <Route path="/events/" component={Events} />
     </div>
   </Router>
 );
